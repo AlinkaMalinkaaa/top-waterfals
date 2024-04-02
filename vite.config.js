ddcf0dcbf35b2eae3e0176f66fe35ce7cda12110
alina <cartoonlina57@gmail.com>
2024-04-03 00:02:44 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base: "/top-waterfals",
+
+    base: "/top-waterfals",
   plugins: [vue()],
 
   resolve: {
